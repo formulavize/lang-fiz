c0753c5a0c496c876dbd909c0963380561c821ea
anthologen <anthologen@gmail.com>
2024-03-16 01:52:27 -0400

    run prettier formatting

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,35 @@
-import { parser } from "@formulavize/lezer-fiz"
+import { parser } from "@formulavize/lezer-fiz";
 import {
-  LanguageSupport, LRLanguage, delimitedIndent,
-  foldInside, foldNodeProp, indentNodeProp
-} from "@codemirror/language"
+  LanguageSupport,
+  LRLanguage,
+  delimitedIndent,
+  foldInside,
+  foldNodeProp,
+  indentNodeProp,
+} from "@codemirror/language";
 
 export const fizLanguage = LRLanguage.define({
   parser: parser.configure({
     props: [
       indentNodeProp.add({
-        ArgList: delimitedIndent({closing: ")", align: false}),
-        StyleArgList: delimitedIndent({closing: "}", align: false})
+        ArgList: delimitedIndent({ closing: ")", align: false }),
+        StyleArgList: delimitedIndent({ closing: "}", align: false }),
       }),
       foldNodeProp.add({
         ArgList: foldInside,
         StyleArgList: foldInside,
-        BlockComment(tree) { return {from: tree.from + 2, to: tree.to - 2} }
-      })
-    ]
+        BlockComment(tree) {
+          return { from: tree.from + 2, to: tree.to - 2 };
+        },
+      }),
+    ],
   }),
   languageData: {
-    closeBrackets: {brackets: ["(", "{", "'", '"']},
-    commentTokens: {line: "//", block: {open: "/*", close: "*/"}},
-  }
-})
+    closeBrackets: { brackets: ["(", "{", "'", '"'] },
+    commentTokens: { line: "//", block: { open: "/*", close: "*/" } },
+  },
+});
 
 export function fiz() {
-  return new LanguageSupport(fizLanguage)
+  return new LanguageSupport(fizLanguage);
 }
